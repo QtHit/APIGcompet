5c08400394728af58a69cf69bc6753e9d2ca7d11
Henri <pnpeepdc@gmail.com>
2022-09-27 17:18:15 +0200

    essai avec waitress

--- API.py
+++ API.py
@@ -132,4 +132,8 @@ def test():
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
              
